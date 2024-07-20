802aed977274a60a910ab58b2772815154b28c2c
FernandoEngler <fernando_engler@estudante.sc.senai.br>
2024-07-20 10:17:15 -0300

    Implementa fluent validation
    Implementa fluent validation

--- Looh.Application/DependencyInjection.cs
+++ Looh.Application/DependencyInjection.cs
@@ -2,6 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 using System.Reflection;
+using Looh.Application.Common.Behaviors;
+using Looh.Application.Authentication.Commands.Register;
+using ErrorOr;
+using Looh.Application.Authentication.Common;
+using FluentValidation;
 
 namespace Looh.Application;
 
@@ -11,6 +16,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).GetTypeInfo().Assembly));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).GetTypeInfo().Assembly);
         return services;
     }
 
